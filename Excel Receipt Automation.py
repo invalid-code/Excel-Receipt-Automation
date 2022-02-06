6ac1a511a6557107258a76df32a8b5f6a55d3c75
invalid-code <jessgaming55@gmail.com>
2022-02-06 15:23:38 +0800

    trying to understand openpyxl

--- Excel Receipt Automation.py
+++ Excel Receipt Automation.py
@@ -1,3 +1,9 @@
 import openpyxl as xl
-with open("_.xlsx", "w") as f:
+wb = xl.Workbook()
+ws = wb.active
+print(wb,ws)
+file_location = "C:\\Users\\JessG\\Documents\\Mom's Work\\test.xlsx"
+
+with open(file_location, "w") as f:
 	f.write("")
+help(xl)

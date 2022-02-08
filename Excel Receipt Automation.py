f2bea65f15998e1f6c7bee7feb8a73da4b05a567
invalid-code <jessgaming55@gmail.com>
2022-02-08 17:36:54 +0800

    merged company name

--- Excel Receipt Automation.py
+++ Excel Receipt Automation.py
@@ -1,5 +1,11 @@
 from openpyxl import Workbook
+from openpyxl.utils import get_column_letter
 wb = Workbook()
 ws = wb.active
+count = 1
 ws["A1"] = "Name of Company" 
+ws.append(["Product Name", "Prev. Inv.", "Pull-Out Stocks", "Total Stocks", "SOH", "Stocks Sold", "Unit", "Amount"])
+while ws[get_column_letter(count) + "2"].value:
+	count += 1
+ws.merge_cells("A1:" + get_column_letter(count) + "1")
 wb.save("C:\\Users\\JessG\\Documents\\Mom's Work\\test.xlsx")
